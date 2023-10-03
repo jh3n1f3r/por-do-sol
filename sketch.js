19c5d501b931a7f736e6a5aafea262a656013b40
jh3n1f3r <146085046+jh3n1f3r@users.noreply.github.com>
2023-10-03 08:39:33 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let posicaoVertical;
+
+function setup() {
+  createCanvas(500, 500);  
+  posicaoVertical = 250;
+}
+
+function draw() {
+  if(posicaoVertical < 400){
+    background("lightblue");
+    posicaoVertical= posicaoVertical
+   +1;
+  }else {
+    background("black");
+  }
+  fill("orange");
+  circle(250,posicaoVertical,300);
+  fill("darkblue");
+  rect(0, 250, 500, 350);
+  fill("khaki");
+  rect(0, 400, 500, 100);
+
+}
